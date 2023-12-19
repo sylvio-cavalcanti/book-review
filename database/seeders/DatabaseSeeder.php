9fbbf41beb23f25ccff0d5ab6c69c435d2863617
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2023-12-19 17:14:08 -0300

    Added fake data to DB using Factory, State Methods and Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Book::factory(33)->create()->each(function ($book){
+            $numReviews = random_int(5, 30); // Number of reviews the book will have
+            
+            Review::factory()->count($numReviews) // Number of reviews to be created 
+                ->good() // Our state method from the factory
+                ->for($book) // This will associate the created review with the Book (Laravel is smart enought)
+                ->create(); // Stores the data to the DB
+        });
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Book::factory(33)->create()->each(function ($book){
+            $numReviews = random_int(5, 30);
+            Review::factory()->count($numReviews) 
+                ->average()
+                ->for($book) 
+                ->create(); 
+        });
+
+        Book::factory(34)->create()->each(function ($book){
+            $numReviews = random_int(5, 30); 
+            
+            Review::factory()->count($numReviews) 
+                ->bad() 
+                ->for($book) 
+                ->create(); 
+        });
     }
 }
